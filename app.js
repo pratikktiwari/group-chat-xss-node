cae98c14f12025888823f69c0e66c410a203d089
Pratik Tiwari <ipratiktiwari@gmail.com>
2025-03-05 16:57:07 +0530

    port 8080

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const sqlite3 = require('sqlite3').verbose(); // Import SQLite3
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 
 // Uncomment for MySQL Database connection
